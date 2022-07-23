a8de90e861dffb028a29fdbb3d3fe35162eba8b6
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-07-23 19:50:03 +0530

    Drum Kit completed

--- index.js
+++ index.js
@@ -2,13 +2,38 @@
 document.querySelectorAll(".drum")[i].addEventListener("click", handClick);
  }
 
+ // Detecting click press
+
 function handClick() {
 
     var sound = this.textContent;
-    console.log(sound);
+    makeSound(sound);
+    buttonAnimation(sound);
+    
+}
 
-    switch(sound){
-        case 'w':
+    // Detecting keyBoard press
+
+document.addEventListener("keypress",function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+    // Adding button Animation
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
+
+
+function makeSound(key) {
+    switch(key){
+        case 'a':
         var audio1 = new Audio('/sounds/tom-1.mp3');
         audio1.play();
         break;
@@ -18,36 +43,33 @@ function handClick() {
         audio2.play();
         break;
 
-        case 'a':
+        case 'd':
         var audio3 = new Audio('/sounds/tom-3.mp3');
         audio3.play();
         break;
 
-        case 'd':
+        case 'f':
         var audio4 = new Audio('/sounds/tom-4.mp3');
         audio4.play();
         break;
 
-        case 'j':
+        case 'g':
         var audio5 = new Audio('/sounds/snare.mp3');
         audio5.play();
         break;
 
-        case 'k':
+        case 'h':
         var audio6 = new Audio('/sounds/kick-bass.mp3');
         audio6.play();
         break;
 
-        case 'l':
+        case 'j':
         var audio7 = new Audio('/sounds/crash.mp3');
         audio7.play();
         break;
 
-        default: alert("Error 404");
+        default: console.log("Error 404");
         
     }
-    
 }
 
-// var audio = new Audio('/sounds/tom-1.mp3');
-//     audio.play();
